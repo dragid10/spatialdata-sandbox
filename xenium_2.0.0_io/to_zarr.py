fc7fa1573f5c13dfc9d84588eca46b7740170ecc
Alex Oladele <dragid10@gmail.com>
2024-12-17 13:41:39 -0500

    Implement argparse to allow for arbitrary file path reads

--- xenium_2.0.0_io/to_zarr.py
+++ xenium_2.0.0_io/to_zarr.py
@@ -1,42 +1,67 @@
+## Convert the data to zarr format
+## Can be run any of the following ways:
 ##
-from spatialdata_io import xenium
-import spatialdata as sd
+## python to_zarr.py
+## python to_zarr.py -d data -o data.zarr (custom)
+## python to_zarr.py --data data --output data.zarr
 
 ##
-from pathlib import Path
+import argparse
 import shutil
+from pathlib import Path
 
-##
-path = Path().resolve()
+import spatialdata as sd
+from spatialdata_io import xenium
+
+## Constants
+ROOT_PROJECT_PATH = Path(__file__).resolve()
+DATA_READ_PATH = ROOT_PROJECT_PATH / "data"
+DATA_WRITE_PATH = ROOT_PROJECT_PATH / "data.zarr"
+
+## Start
+
+# Use argparse to allow runing script as cli tool
+parser = argparse.ArgumentParser(description="Convert data to zarr format")
+parser.add_argument(
+    "-d"
+    "--data",
+    type=str,
+    default=str(DATA_READ_PATH),
+    help="Path to the data to be converted", )
+parser.add_argument("-o",
+                    "--output",
+                    type=str,
+                    default=str(DATA_WRITE_PATH),
+                    help="Path to where you want to save the data", )
+args = parser.parse_args()
+
 # luca's workaround for pycharm
-if not str(path).endswith("xenium_2.0.0_io"):
-    path /= "xenium_2.0.0_io"
-    assert path.exists()
+# if not str(path).endswith("xenium_2.0.0_io"):
+#     path /= "xenium_2.0.0_io"
+#     assert path.exists()
 
-path_read = path / "data"
-path_write = path / "data.zarr"
-
-##
-print("parsing the data... ", end="")
+## Parse the data
+print(f"parsing the data file: {DATA_READ_PATH} ... ", end="")
 sdata = xenium(
-    path=str(path_read),
+    path=str(DATA_READ_PATH),
+    n_jobs=8,
     cell_boundaries=True,
     nucleus_boundaries=True,
     morphology_focus=True,
     cells_as_circles=True,
 #    cleanup_labels_zarr_tmpdir=False,
 )
-print("done")
+print("Data file successfully parsed")
+
+## Write transformed data to zarr
+print(f"writing the data to: {str(DATA_WRITE_PATH)} ... ", end="")
+if DATA_WRITE_PATH.exists():
+    shutil.rmtree(DATA_WRITE_PATH)
+sdata.write(DATA_WRITE_PATH)
+print(f"Successfully wrote the data to: {str(DATA_WRITE_PATH)}")
 
 ##
-print("writing the data... ", end="")
-if path_write.exists():
-    shutil.rmtree(path_write)
-sdata.write(path_write)
-print("done")
-
-##
-sdata = sd.SpatialData.read("./data.zarr/")
+sdata = sd.SpatialData.read(f"{str(DATA_WRITE_PATH)}/")
 print(sdata)
 print(sdata['transcripts']['feature_name'].compute())
 ##
